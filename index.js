dac8d3ca0e39843af37675695bf5736382d36c31
derek chen <derek.g.chen@gmail.com>
2018-03-05 15:46:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,35 +1,70 @@
-
-
 function theBeatlesPlay(musicians, instruments){
-  var i =0;
-  var result =[];
-  for(;musicians[i];i++){
-    result[i] =musicians[i] + " plays " + instruments[i];
+  var temp=[]
 
+  for( var i= 0; i < musicians.length; i++){
+    temp.push( musicians[i] + " plays " + instruments[i] )
   }
-  //console.log(result);
-  return result;
+  return temp
+
 }
 
-
-function johnLennonFacts (facts){
-  var result = [];
+function johnLennonFacts(facts){
   var i =0;
-  while(facts[i]){
-    result[i] = facts[i] + "!!!";
+  var temp = [];
+  while (i<facts.length){
+    temp.push (facts[i] + "!!!");
     i++;
   }
-  return result;
+  return temp
+
 }
 
 
-function iLoveTheBeatles(deep){
-  var result =[];
+function iLoveTheBeatles(n){
+  var temp =[];
   var i =0;
   do{
-    result[i] = "I love the Beatles!";
-    i++;
-  }while(i <= deep && deep < 15);
-  return result;
+    temp.push("I love the Beatles!")
+    i++
+  }while(n<15 && i<n+1)
+  return temp
 
 }
+
+
+
+// //////before
+// 
+// function theBeatlesPlay(musicians, instruments){
+//   var i =0;
+//   var result =[];
+//   for(;musicians[i];i++){
+//     result[i] =musicians[i] + " plays " + instruments[i];
+// 
+//   }
+//   //console.log(result);
+//   return result;
+// }
+// 
+// 
+// function johnLennonFacts (facts){
+//   var result = [];
+//   var i =0;
+//   while(facts[i]){
+//     result[i] = facts[i] + "!!!";
+//     i++;
+//   }
+//   return result;
+// }
+// 
+// 
+// function iLoveTheBeatles(deep){
+//   var result =[];
+//   var i =0;
+//   do{
+//     result[i] = "I love the Beatles!";
+//     i++;
+//   }while(i <= deep && deep < 15);
+//   return result;
+// 
+// }
